530073e3754186f5bbd1678b9723a5fc9a779425
abstract12 <64096872+abstract12@users.noreply.github.com>
2022-12-24 16:28:10 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://aladdin.onrender.com/', {
+    const response = await fetch('https://aladdin-s4n3.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
